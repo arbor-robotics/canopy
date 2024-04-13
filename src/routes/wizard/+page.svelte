39f790bd144d5e950127bbad862478772fa8c0dc
Will Heitman <16737488+wheitman@users.noreply.github.com>
2024-04-13 00:21:36 -0400

    Add to Wizard - Install Melt UI - Install Sveltestrap

--- src/routes/wizard/+page.svelte
+++ src/routes/wizard/+page.svelte
@@ -5,8 +5,16 @@
   import Icon from "$lib/misc/Icon.svelte";
   import "@material/web/progress/circular-progress";
   import "@material/web/button/text-button";
+  import "@material/web/button/elevated-button";
   import "@material/web/fab/fab";
   import "@material/web/icon/icon";
+  import {
+    Button,
+    Styles,
+    Toast,
+    ToastBody,
+    ToastHeader,
+  } from "@sveltestrap/sveltestrap";
 
   import ConnectionIndicator from "$lib/ros/ConnectionIndicator.svelte";
   import { connection_status, ConnectionStatus, node } from "$lib/stores";
@@ -17,31 +25,69 @@
   let geojson = undefined;
   let forest_plan_geojson_topic = undefined;
 
+  onMount(async () => {
+    const bootstrap = await import("bootstrap");
+  });
+
   node.subscribe((node) => {
     forest_plan_geojson_topic = new ROSLIB.Topic({
       ros: node,
       name: "/planning/bounds_geojson",
       messageType: "std_msgs/String",
     });
-
-    var json_msg = {
-      data: "YEET",
-    };
-
-    // forest_plan_geojson_topic.publish(json_msg);
   });
 
   let count = 0;
   let osmMap;
+  let toast_header_text;
+  let toast_body_text;
+  let boundsOK = false;
   function publishPlantingPlan() {
-    //incremtent
-    count++;
-    console.log(JSON.stringify(osmMap.getGeoJSON().geometry));
+    if (osmMap.getGeoJSON() == undefined) {
+      showErrorToast(
+        "No bounds",
+        "Trying drawing your planting area using the brush button in the top right."
+      );
+      return;
+    }
+
+    let geometry = osmMap.getGeoJSON().geometry;
+
+    if (geometry.coordinates.length > 1) {
+      showErrorToast(
+        "Bounds should be contiguous",
+        "Make sure that your bounds are a single, connected shape"
+      );
+      return;
+    }
+
     geojson = JSON.stringify(osmMap.getGeoJSON().geometry);
+    console.log(osmMap.getGeoJSON().geometry);
     var json_msg = {
       data: geojson,
     };
     forest_plan_geojson_topic.publish(json_msg);
+    boundsOK = true;
+  }
+
+  let isToastOpen = false;
+
+  function toggleToast() {
+    isToastOpen = !isToastOpen;
+  }
+
+  function openToast() {
+    isToastOpen = true;
+  }
+
+  function onToastClose() {
+    isToastOpen = false;
+  }
+
+  function showErrorToast(title: string, description: string) {
+    toast_header_text = title;
+    toast_body_text = description;
+    openToast();
   }
 </script>
 
@@ -49,6 +95,8 @@
   <title>Canopy</title>
   <meta name="description" content="About this app" />
 </svelte:head>
+
+<Styles />
 
 <div
   class="wizard-steps shadow-xl py-2 rounded-lg flex-row flex justify-around align-middle"
@@ -67,64 +115,31 @@
   </div>
   <div class="opacity-35">Start</div>
 </div>
-<!-- <World /> -->
-<OsmMap bind:this={osmMap} />
-<!-- <MessageBox /> -->
+
+{#if boundsOK}
+  <World />
+{:else}
+  <OsmMap bind:this={osmMap} />
+{/if}
 
 <div class="absolute bottom-0 right-0 pb-4 pr-4">
-  <button on:click={publishPlantingPlan}
-    ><md-fab aria-label="Edit">
-      <md-icon slot="icon">play_arrow</md-icon>
-    </md-fab></button
-  >
+  <button on:click={publishPlantingPlan}>
+    <md-elevated-button class="flex flex-row"> Done</md-elevated-button>
+  </button>
 </div>
 
-<!-- 
-<div id="wizard-container">
-  <md-circular-progress
-    id="wizard-progress"
-    value={progressValue}
-    indeterminate={progressIsIndeterminate}
-  ></md-circular-progress>
-  <md-text-button
-    id="wizard-button"
-    trailing-icon
-    on:click={buttonClicked}
-    on:keydown={buttonClicked}
-    role="button"
-    tabindex="0"
-  >
-    <Icon
-      id="play_arrow"
-      color="var(--md-sys-color-on-surface-variant)"
-      size="8rem"
-      fill="0"
-    ></Icon>
-  </md-text-button>
-  <p id="wizard-label">Steward is ready</p>
-</div> -->
-
-<!-- <div class="text-column">
-	<h1>About this app</h1>
-
-	<p>
-		This is a <a href="https://kit.svelte.dev">SvelteKit</a> app. You can make your own by typing the
-		following into your command line and following the prompts:
-	</p>
-
-	<pre>npm create svelte@latest</pre>
-
-	<p>
-		The page you're looking at is purely static HTML, with no client-side interactivity needed.
-		Because of that, we don't need to load any JavaScript. Try viewing the page's source, or opening
-		the devtools network panel and reloading.
-	</p>
-
-	<p>
-		The <a href="/sverdle">Sverdle</a> page illustrates SvelteKit's data loading and form handling. Try
-		using it with JavaScript disabled!
-	</p>
-</div> -->
+<div
+  class="absolute bottom-0 right-auto left-0 flex flex-row w-full justify-center pb-1"
+>
+  <Toast isOpen={isToastOpen} autohide duration={100} on:close={onToastClose}>
+    <ToastHeader toggle={toggleToast} icon={"danger"}
+      >{toast_header_text}</ToastHeader
+    >
+    <ToastBody>
+      {toast_body_text}
+    </ToastBody>
+  </Toast>
+</div>
 
 <style>
   .wizard-steps {
